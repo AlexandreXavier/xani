b235a8f68e993d61e0096735797a9f3e063e8f4c
AlexandreXavier <jalexandre.xavier@gmail.com>
2025-03-01 20:18:14 +0000

    first commit for the podcast

--- src/content/config.ts
+++ src/content/config.ts
@@ -23,4 +23,21 @@ const blog = defineCollection({
     }),
 });
 
-export const collections = { blog };
+const episodeSchema = z.object({
+  title: z.string(),
+  audioUrl: z.string(),
+  pubDate: z.coerce.date().optional(),
+  cover: z.string().optional(),
+  explicit: z.boolean().optional(),
+  episode: z.number().optional(),
+  season: z.number().optional(),
+  episodeType: z.string().optional(),
+  duration: z.coerce.string(), //duration in format hh:mm:ss
+  size: z.number(), // size in megabytes
+});
+
+export type episodeSchema = z.infer<typeof episodeSchema>;
+
+const episodeCollection = defineCollection({ schema: episodeSchema });
+
+export const collections = { blog,'episode': episodeCollection, };
