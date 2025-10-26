de5f54e9893b77b90701c42aee4758a77e98722f
alexandrexavier <jalexandre.xavier@gmail.com>
2025-10-26 08:08:05 +0000

    new test to fixed

--- src/pages/og.png.ts
+++ src/pages/og.png.ts
@@ -1,7 +1,15 @@
 import type { APIRoute } from "astro";
 import { generateOgImageForSite } from "@utils/generateOgImages";
 
-export const GET: APIRoute = async () =>
-  new Response(await generateOgImageForSite(), {
-    headers: { "Content-Type": "image/png" },
+export const GET: APIRoute = async () => {
+  const buffer = await generateOgImageForSite();
+  
+  // Create a Response with the Buffer using a type assertion
+  return new Response(buffer as unknown as BodyInit, {
+    headers: { 
+      'Content-Type': 'image/png',
+      'Content-Length': buffer.length.toString(),
+      'Cache-Control': 'public, max-age=31536000, immutable'
+    }
   });
+};
