6cdab2cdd5b948e64d372a69053a7f86675e58b2
Alexandre Xavier <jalexandre.xavier@gmail.com>
2025-11-06 19:36:53 +0000

    new

--- src/utils/episodeFilter.ts
+++ src/utils/episodeFilter.ts
@@ -1,7 +1,15 @@
 import { SITE } from "@config";
 import type { CollectionEntry } from "astro:content";
 
-const episodeFilter = ({ data }: CollectionEntry<"blog">) => {
+// Define the type for the blog post data
+interface BlogPostData {
+  data: {
+    pubDatetime: Date;
+    draft?: boolean;
+  };
+}
+
+const episodeFilter = ({ data }: BlogPostData) => {
   if (!data.pubDatetime) return false;
   
   const publishTime = new Date(data.pubDatetime).getTime();
